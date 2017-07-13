a3fc0a8be11e8d962c3087f025417d3c799eb122
ahmadnourallah <ahmadnurallah@gmail.com>
2017-07-12 22:11:33 -0400

    upload samples folder

---
+++ samples/simple_sample.py
@@ -0,0 +1,21 @@
+info = {
+        'author'            :'Creator name',
+        'date'              :'Create date',
+        'rank'              :'Rank of the module',
+        'category'          :'Main category of the module',
+        'path'              :'Module create path',
+        'license'           :'Module license',
+        'description'       :'Module description',
+        'references'        :['References for further reading']
+}
+options = {
+            'options_name'        :['require', 'description','value']
+}
+required = {
+        'start_required'     :'False/True', # using default starter
+        'check_required'     :'False/True' # module support on not support check option
+}
+def exploit(): # main exploit function
+    pass # stuff to do
+def check(): # check function if the module require it
+    pass # stuff to do
