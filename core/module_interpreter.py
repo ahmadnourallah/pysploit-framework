be5d4221972e28deb005c1cbfd24481ba4bcf0ad
Ahmad Nourallah <ahmadnurallah@gmail.com>
2017-07-20 18:00:55 -0400

    add clear command to completer

--- core/module_interpreter.py
+++ core/module_interpreter.py
@@ -27,7 +27,7 @@ def module_completer():
 
             self.listCompleter = listCompleter
     t = tabCompleter()
-    t.createListCompleter(["set", "exploit", "back", "check", "help", "info", 'banner', 'run', 'exec'])
+    t.createListCompleter(["set", "exploit", "back", "check", "help", "info", 'banner', 'run', 'exec','clear'])
     readline.set_completer_delims('\t')
     readline.parse_and_bind("tab: complete")
     readline.set_completer(t.listCompleter)
